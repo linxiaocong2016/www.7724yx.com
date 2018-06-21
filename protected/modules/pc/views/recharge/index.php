561ff935b4a125a849be5d411c637ea1ec201f47
chenjielang <KiriJayla@gmail.com>
2018-06-21 17:11:45 +0800

    充值中心页面

--- protected/modules/pc/views/recharge/index.php
+++ protected/modules/pc/views/recharge/index.php
@@ -1,112 +1,147 @@
-<script>
-$(function(){
-	$(".exchange_bt").click(function(){
-		var li_hidden=$(this).closest('.recommend_list').find('ul li:hidden');
-		var li=$(this).closest('.recommend_list').find('ul li');
-		if(li_hidden.length>0){
-			$(li).hide();
-			$(li_hidden).show();
-		}
-		
-	});
-})
-</script>
+<style>
+.recharge-box {
+	width: 1140px;
+	height:752px;
+	background: #fff;
+	margin: 0 auto;
+}
+
+.recharge-title {
+	margin: 0 21px;
+	height: 50px;
+	line-height: 50px;
+	font-size: 22px;
+	font-weight: normal;
+	border-bottom: 1px solid #e5e5e5; 
+}
+.recharge-title span {
+	display: inline-block;
+	border-bottom: 3px solid #0685f6;
+}
+.recharge-type {
+	margin: 30px 21px;
+	font-size: 16px;
+} 
+.recharge-type>div {
+	display: inline-block;
+	width: 160px;
+	height:160px;
+	border: 1px solid #ddd;
+	border-radius: 20px;
+	margin:20px;
+	cursor: pointer;
+}
+.recharge-type>.active {
+	border: 1 solid transparent;
+	background: url(/static/img/border.png) no-repeat;
+}
+.recharge-num {
+	margin: 0 21px;
+	font-size: 16px;
+}
+.num-input-box {
+	margin: 15px 0;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+	background: #f4f4f4;
+	width: 390px;
+	height:38px;
+	padding-left: 10px;
+}
+.recharge-num input {
+	outline: none;
+	-webkit-appearance: none;
+	display: inline-block;
+	border: 0;
+	width: 390px;
+	height: 38px;
+	background: #f4f4f4;
+}
+.num-show {
+	width: 400px;
+	font-size: 14px;
+	color: #737373;
+}
+#qibiNum {color: #f3592f;}
+
+.recharge-btn {
+	display: inline-block;
+	width: 250px;
+	height: 38px;
+	line-height: 38px;
+	text-align: center;
+	border-radius: 5px;
+	background: #009fe8;
+	color: #fff;
+	margin: 30px 21px;
+	font-size: 16px;
+	cursor: pointer;
+}
+</style>
 <div class="index_center">
 	<div class="general">
 		<!-- 当前位置 -->
 		<div class="index_pos">
 			<img src="/img/local_ico.png">
-			<span class="pos_text">当前位置:<a>首页</a>>手游</span>
+			<span class="pos_text">当前位置:<a>首页</a>>充值中心</span>
 		</div>
-		<!-- 热门游戏 -->
-		
-		<div class="index_hot">
-			<div class="index_title_box">
-				<div class="index_title">热门游戏</div>
-			</div>
-			<div class="hot_list_box">
-				<div class="hot_item">
-					<a target="_blank" title='雪鹰领主' href="/pc/game/gamedetail?gameid=37">
-						<div class="hot_img_box">
-							<img class="hot_img"src="/img/hot_1.jpg">
-						</div>			
-						<div class="hot_select_box">
-							<img class="hot_select_icon" width="60" height="60" src="http://upload.92fox.com/201608/ba802fb96a47ffd479d8676ab12dc57e.png">
-							<p class="hot_select_msg">手游 | 仙侠</p>
-							<p class="hot_select_msg">评分:<span class="hot_star"></span></p>
-							<p class="hot_select_msg">人气:2254225</p>
-						</div>
-					</a>			
-					<p class="hot_item_name">梦幻西游</p>
-				</div>	
-				<div class="hot_item">
-					<a target="_blank" title='' href="/pc/game/gamedetail?gameid=29">
-						<div class="hot_img_box">
-							<img class="hot_img"src="/img/hot_2.jpg">
-						</div>			
-						<div class="hot_select_box">
-							<img class="hot_select_icon" width="60" height="60" src="http://upload.92fox.com/201701/06b7bf55d8ada67bfc58b4a35ebc1ebc.png">
-							<p class="hot_select_msg">手游 | 仙侠</p>
-							<p class="hot_select_msg">评分:<span class="hot_star"></span></p>
-							<p class="hot_select_msg">人气:3546887</p>
-						</div>
-					</a>			
-					<p class="hot_item_name">镇魔曲</p>
-				</div>
-				<div class="hot_item">
-					<a target="_blank" title='' href="/pc/game/gamedetail?gameid=34">
-						<div class="hot_img_box">
-							<img class="hot_img"src="/img/hot_3.jpg">
-						</div>			
-						<div class="hot_select_box">
-							<img class="hot_select_icon" width="60" height="60" src="http://image.92fox.com/92fox/game/ac/ac9b10e3066ba30428d3cf125e0e6a23.jpg">
-							<p class="hot_select_msg">手游 | 仙侠</p>
-							<p class="hot_select_msg">评分:<span class="hot_star"></span></p>
-							<p class="hot_select_msg">人气:2678994</p>
-						</div>
-					</a>			
-					<p class="hot_item_name">倩女幽魂</p>
-				</div>	
-				<div class="hot_item">
-					<a target="_blank" title='' href="/pc/game/gamedetail?gameid=31">
-						<div class="hot_img_box">
-							<img class="hot_img"src="/img/hot_4.jpg">
-						</div>			
-						<div class="hot_select_box">
-							<img class="hot_select_icon" width="60" height="60" src="http://upload.92fox.com/201701/d9d584cd8e287ea06422059611de8b60.png">
-							<p class="hot_select_msg">手游 | 仙侠</p>
-							<p class="hot_select_msg">评分:<span class="hot_star"></span></p>
-							<p class="hot_select_msg">人气:3546551</p>
-						</div>
-					</a>			
-					<p class="hot_item_name">大唐无双</p>
-				</div>	
-			</div>		
-		</div>
-
-		<!-- 游戏列表 -->
-		<div class="index_gamelist">
-			<div class="index_title_box">
-				<div class="index_title">游戏列表</div>
-			</div>
-			<div id="gameListBox" class="gamelist_box">
-				
-			</div>
-		</div>
-		<!-- 页码列 -->
-			<div class="pagelist_box no_select">
-				<span id="page_count"></span>
-				<div id="first_page_btn" class="page_btn">首页</div>
-				<div id="pre_page_btn" class="page_btn">上一页</div>
-				<ul id="page_list" class="pagelist"></ul>
-				<div id="next_page_btn" class="page_btn">下一页</div>
-				<div id="last_page_btn" class="page_btn">末页</div>
-				
-			</div>			
 	</div>
-</div>
+	<!-- 充值中心 -->	
+	<div class="recharge-box">
+		<div class="recharge-title"><span>充值中心</span></div>
+		<div class="recharge-type">
+			<span>选择充值方式：</span></br>
+			<div id="weixinPay" class="active">
+				<img src="/static/img/weixin.png" alt="">
+			</div>
+			<div id="aliPay">
+				<img src="/static/img/alipay.png" alt="">
+			</div>
+		</div>
+		<div class="recharge-num">
+			<span>充值奇币为：</span></br>
+			<div class="num-input-box">	
+				<input id="payNum" type="text" placeholder="请输入充值金额" oninput="calQibiNum(this.value)">
+			</div>	
+			<div class="num-show">
+				奇币：<span id="qibiNum">0</span>
+				<span style="float: right;">1元=100奇币</span>
+			</div>
+		</div>
+		<div class="recharge-btn" onclick="submitPay()">
+			提交支付
+		</div>
+	</div>
+<div>
 <script>
-	var page = new PageList();
-	page.init();
+function calQibiNum(value) {
+	var num = value * 100;
+	document.getElementById('qibiNum').innerText = num;
+}
+
+function submitPay() {
+	var num = document.getElementById('payNum').value;
+	if(num=='' || num == '0') {
+		alert('请输入充值金额!');
+	} else {
+		// 此处发起充值请求
+	}
+}
+
+(function(){
+	var payType = 'weixin';
 	
+	var weixinDom = document.getElementById('weixinPay');
+	var aliDom = document.getElementById('aliPay');
+	weixinDom.addEventListener('click',function() {
+		payType = 'weixin';
+		aliDom.className = '';
+		weixinDom.className = 'active';
+	});
+	aliDom.addEventListener('click',function() {
+		payType = 'ali';
+		aliDom.className = 'active';
+		weixinDom.className = '';
+	});
+})();
 </script>
