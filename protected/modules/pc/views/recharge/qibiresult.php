3a074f910fc6a6543b2c3dc541b2e9f0a7609e4d
chenjielang <KiriJayla@gmail.com>
2018-06-25 15:43:20 +0800

    增加支付成功返回页

--- protected/modules/pc/views/recharge/qibiresult.php
+++ protected/modules/pc/views/recharge/qibiresult.php
@@ -1,53 +1,40 @@
 <style>
     .re_result{margin-top:10px;}
-    .re_result .p1{margin:0 8px;}
-    .re_result .p1 span{ display:inline-block; color:#333; padding-left:30px; line-height:30px; font-weight:800;}
+    .re_result .p1{margin:38px 0 10px 0;text-align: center;}
+    .re_result .p1 span{ color:#333; font-size: 27px;}
     .re_result .p1 span.ok{background:url(/img/tishi5.png) no-repeat left center; background-size:20px auto;}
     .re_result .p1 span.fail{background:url(/img/tishi4.png) no-repeat left center; background-size:20px auto;}
-    .re_result .p2,.re_result .p4{ border-bottom:1px solid #e5e5e5; line-height:40px;margin:0 8px; padding-bottom:10px;}
+    .re_result .p2,.re_result .p4{ line-height:20px;margin:0 8px; text-align: center;}
     .re_result .p2 span{color:#ec5b56}
     .re_result a{color:#fff;}
 	.re_result a:hover{ background:#52b33e;}
-    .re_result .p3{ background:#f5f5f5; margin-top:20px; padding:0 8px; line-height:30px; height:30px; font-size:12px; color:#999; clear:both;}
+    .re_result .p3{ margin: 20px auto;width: 500px;height:67px;line-height: 67px;text-align: center;background:#f5f5f5;font-size:16px; color:#999;}
     .re_result .p4{color:#9c9c9c;}
     .new_shadow{border-bottom:1px solid rgba(223,223,223,0.8);box-shadow:0 2px 0 rgba(227,227,227,0.2);background:#fff;padding:20px 0 0 0;font-size:14px;}
-    
+    .re_result .result_img {margin: 0 auto;width: 248px;height: 248px;}
+    .recharge_button2 {display:block;background: #00b3ff;border-radius: 5px;text-decoration: none;margin: 0 auto;width: 200px;height: 40px;line-height: 40px;color: #fff;text-align: center;font-size: 15px;}
 </style>
 
 <div style="margin-top: 35px;">
     <?php if($result['success']=='1'):?>
         <!--充值成功-->            
 		
-		
-        <?php if(isset($result['game_spend']) && $result['game_spend']==1):?>
-        <div class="re_result new_shadow">            
-            <p class="p1"><span class="ok">成功充值 <?php echo $result['amount'] ? $result['amount'] . "元！" : ""; ?></span></p>
-            <p class="p4"></p> 
-            
-            <a class="recharge_button2" href="<?php echo $result['game_url']?>">返回游戏</a>
+        <div class="re_result">            
+            <p class="result_img"><img src="/static/img/payResult" ></p>
+            <p class="p1"><span class="ok">成功充值 <?php echo "<span style='color:#ff530f;'>".($result['amount'] ? $result['amount'] : "100")."</span> 元"; ?></span></p>
+            <p class="p4">订单号为</p> 
             
             <p class="p3">注：如充值遇到问题，请联系客服QQ：2885339244</p>
+            <a class="recharge_button2" href="javascript:history.go(-1)">好的,我知道了</a>
         </div> 
-        
-        <?php else:?>
-        <div class="re_result new_shadow">            
-            <p class="p1"><span class="ok">成功充值<?php echo $result['amount'] ? $result['amount'] . "元！" : ""; ?></span></p>
-            <p class="p4"></p> 
-            
-            
-            <p class="p3">注：如充值遇到问题，请联系客服QQ：2885339244</p>
-        </div> 
-		<?php endif;?>
 		
     <?php elseif($result['success']=='2'):?>
         <!--页面跳转奇币支付错误-->            
-        <div class="re_result new_shadow">            
+        <div class="re_result ">            
             <p class="p1"><span class="fail"><?php echo $result['msg']; ?></span></p>
             <p class="p4"></p> 
-            
-            <a class="recharge_button2" href="javascript:history.go(-1)">返回</a>
-            
             <p class="p3">注：如充值遇到问题，请联系客服QQ：2885339244</p>
+            <a class="recharge_button2" href="javascript:history.go(-1)">好的,我知道了</a>
         </div> 
     
     <?php elseif($result['success']=='3'):?>
@@ -55,7 +42,7 @@
     	<style>a.back{display: none;}</style>
     	
         <!--正式进行奇币支付错误-->            
-        <div class="re_result new_shadow">            
+        <div class="re_result">            
             <p class="p1"><span class="fail"><?php echo $result['msg']; ?></span></p>
             <p class="p4"></p> 
             
@@ -68,7 +55,7 @@
     	<style>a.back{display: none;}</style>
     	
         <!--奇币支付成功-->            
-        <div class="re_result new_shadow">            
+        <div class="re_result">            
             <p class="p1"><span class="ok"><?php echo $result['msg']; ?></span></p>
             <p class="p4"></p> 
             
@@ -79,7 +66,7 @@
         
     <?php else: ?>
         <!--充值失败 -->            
-        <div class="re_result new_shadow">
+        <div class="re_result">
             <p class="p1"><span class="fail">充值失败！</span></p>      
             <p class="p4"><?php echo $result['msg'] ? $result['msg'] : "请检查网络是否畅通?" ?></p>  
             
