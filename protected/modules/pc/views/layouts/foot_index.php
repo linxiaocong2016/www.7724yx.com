1d8e7a6fa0c743aa93d4febd1764628f368421df
Kirios <KiriJayla@gmail.com>
2018-11-28 12:01:45 +0800

    底部栏增加文本

--- protected/modules/pc/views/layouts/foot_index.php
+++ protected/modules/pc/views/layouts/foot_index.php
@@ -2,7 +2,7 @@
  <div class="h5_footer">
     <div class="general"  style="white-space: nowrap;text-align: center;">
         <br />
-       <p style="display: inline-block;">广州七七游网络科技有限公司 版权所有 | Copyright @ 2015-2020 | <a href="http://www.miitbeian.gov.cn/publish/query/indexFirst.action" style="text-decoration:none;color: #fff;">粤ICP备18013779号-1</a></p>
+       <p style="display: inline-block;">广州七七游网络科技有限公司 版权所有 | Copyright @ 2015-2020 | <a href="http://www.miitbeian.gov.cn/publish/query/indexFirst.action" style="text-decoration:none;color: #fff;">粤ICP备18013779号-1</a><br>增值电信业务经营许可证：粤B2-20181261</p>
    </div>
    <div style="width: 1100px;padding:20px 0;margin: 0 auto;white-space: nowrap;text-align:center;">
       <img src='/assets/1.png' /><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44010602003743" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44010602003743号</p></a>&nbsp;&nbsp;<a target="__blank" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;" href="http://sq.ccm.gov.cn:80/ccnt/sczr/service/business/emark/toDetail/fe0c68258f6546d68f90fb4f4327d53b"><img style="float:left;width:25px;height:25px;" src="assets/wenhuajingying.png" /><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤网文〔2018〕2196-775号</p></a>
